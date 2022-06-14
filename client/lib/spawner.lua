1f62f51cddb164ce203262e9e07ed62181b45f77
napitek <napitek@protonmail.com>
2022-06-14 17:49:35 +0200

    Empty Main

--- client/lib/spawner.lua
+++
@@ -1,2 +0,0 @@
-local j = nil
-
--- config.lua
+++
@@ -1,9 +0,0 @@
-Config = {}
-
-Config.AnimalPedPrefix = "a_c_"
-Config.DefaultScenario = "WORLD_HUMAN_SMOKING"
-
-Config.Teams = {"allies", "enemies", "admins"}
-Config.NotifyBackground = "#444444"
-Config.NotifyPrimaryColor = "#ff4081"
-Config.NotifyTextColor = "#ffffff"
